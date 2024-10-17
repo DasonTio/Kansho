c44bc80adaf789e5cdcfb22bb87049578ec28ea1
DasonTiovino <114289111+DasonTio@users.noreply.github.com>
2024-10-17 22:59:49 +0700

    Initial Commit

---
+++ Kansho/KanshoApp.swift
@@ -0,0 +1,17 @@
+//
+//  KanshoApp.swift
+//  Kansho
+//
+//  Created by Dason Tiovino on 17/10/24.
+//
+
+import SwiftUI
+
+@main
+struct KanshoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ KanshoTests/KanshoTests.swift
@@ -0,0 +1,17 @@
+//
+//  KanshoTests.swift
+//  KanshoTests
+//
+//  Created by Dason Tiovino on 17/10/24.
+//
+
+import Testing
+@testable import Kansho
+
+struct KanshoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
