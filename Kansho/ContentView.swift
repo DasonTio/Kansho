c56eeea9490ce1e057515b0fe9d7d185b2b660be
DasonTiovino <114289111+DasonTio@users.noreply.github.com>
2024-11-03 21:51:50 +0700

    feat: Page Snapping

--- Kansho/ContentView.swift
+++ Kansho/ContentView.swift
@@ -12,35 +12,54 @@ struct ContentView: View {
     @StateObject var relaxViewModel: RelaxViewModel = .init(
         hapticManager: .init()
     )
+    @State private var currentPage = 1
     
     var body: some View {
         GeometryReader{ geometry in
             let height = geometry.size.height / 100
-           
+            
             Color.background
             NavigationStack{
-                ZStack{
-                    RelaxView()
-                        .environmentObject(relaxViewModel)
-                        .offset(
-                            x: 0,
-                            y: relaxViewModel.isJournaling ? height * -100 : 0
+                ScrollViewReader { proxy in
+                    ScrollView{
+                        VStack(spacing: 0){
+                            RelaxView()
+                                .environmentObject(relaxViewModel)
+                                .frame(height: geometry.size.height)
+                                .animation(
+                                    .easeInOut,
+                                    value: relaxViewModel.isJournaling
+                                )
+                                .id(1)
+                            
+                            JournalView()
+                                .environmentObject(relaxViewModel)
+                                .environmentObject(journalViewModel)
+                                .frame(height: geometry.size.height)
+                                .animation(
+                                    .easeInOut,
+                                    value: relaxViewModel.isJournaling
+                                )
+                                .id(2)
+                        } .onChange(of: currentPage) { newPage in
+                            // Programmatically snap to the page when currentPage changes
+                            withAnimation {
+                                proxy.scrollTo(newPage, anchor: .top)
+                            }
+                        }
+                        .gesture(
+                            DragGesture()
+                                .onEnded { value in
+                                    // Detect vertical swipes
+                                    if value.translation.height < 0 { // Swipe up
+                                        currentPage = min(currentPage + 1, 2)
+                                    } else if value.translation.height > 0 { // Swipe down
+                                        currentPage = max(currentPage - 1, 1)
+                                    }
+                                }
                         )
-                        .animation(.easeInOut, value: relaxViewModel.isJournaling)
-                    
-                    JournalView()
-                        .environmentObject(relaxViewModel)
-                        .environmentObject(journalViewModel)
-                        .offset(
-                            x: 0,
-                            y: relaxViewModel.isJournaling ? 0: height * 100
-                        )
-                        .animation(
-                            .easeInOut,       
-                            value: relaxViewModel.isJournaling
-                        )
+                    }
                 }
-
             }
         }
         .ignoresSafeArea()
--- Kansho/Modules/Journal/View/JournalView.swift
+++ Kansho/Modules/Journal/View/JournalView.swift
@@ -15,105 +15,75 @@ struct JournalView: View {
     @State private var cancellables: [AnyCancellable] = []
     
     var body: some View {
-        GeometryReader{ geometry in
-            let height = geometry.size.height / 100
-            let componentActive = showTimerNotification && relaxViewModel.isActive
+        VStack (alignment: .leading, spacing: 0){
+            JournalNotificationButton(image: relaxViewModel.plantImage){
+                // TODO: Trigger Relax
+                print("RELAX...")
+            }
             
-            VStack (alignment: .leading, spacing: 0){
-                // MARK: Journaling
+            // MARK: Add Journal Button
+            Button(action: {
+                // TODO: Call Add Journal
+                journalViewModel.addJournal(JournalModel(
+                    title: "Title",
+                    content: "Content")
+                )
+            }){
+                RoundedRectangle(cornerSize: CGSize(width: 30, height: 30))
+                    .fill(.white)
+                    .strokeBorder(style: StrokeStyle(
+                        lineWidth: 2,
+                        dash: [5])
+                    )
+                    .foregroundStyle(.appSecondary.opacity(0.4))
+                    .overlay {
+                        Image(systemName: "plus")
+                            .font(.themeTitle3())
+                            .foregroundStyle(.appSecondary)
+                    }
+                    .frame(height: 100)
+                    .padding(.horizontal, 15)
+                    .padding(.top, 10)
+            }
+            
+            // MARK: List All Journal
+            ForEach(journalViewModel.data, id: \.self){ journal in
                 Button(action: {
-                    relaxViewModel.isJournaling.toggle()
-                }){
-                    RoundedRectangle(cornerSize: CGSize(width: 30, height: 30))
-                        .fill(.appSecondary)
-                        .overlay {
-                            HStack{
-                                VStack(alignment: .leading){
-                                    Text("Relax Session")
-                                        .font(.themeHeadline(weight: .heavy))
-                                        .foregroundStyle(.appPrimary)
-                                    
-                                    Text("Let's calm down and unwind")
-                                        .font(.themeBody())
-                                        .foregroundStyle(.appPrimary)
-                                }
-                                
-                                Spacer()
-                                
-                                Image(relaxViewModel.plantImage)
-                                    .resizable()
-                                    .scaledToFit()
-                                    .animation(.easeInOut, value: relaxViewModel.plantImage)
-                            }
-                            .padding(.horizontal,20)
-                            .clipped()
-                            .cornerRadius(30)
-                        }
-                        .opacity(componentActive ? 1 : 0)
-                        .frame(
-                            height: componentActive ? height * 15 : 0
-                        )
-                        .animation(
-                            .timingCurve(.circularEaseInOut, duration: 0.5),
-                            value: componentActive
-                        )
-                        .padding(.horizontal, 15)
-                        .padding(.top, 47)
                     
-                }
-                
-                // MARK: Add Journal Button
-                Button(action: {
-                    // TODO: Call Add Journal
                 }){
                     RoundedRectangle(cornerSize: CGSize(width: 30, height: 30))
-                        .fill(.white)
-                        .strokeBorder(style: StrokeStyle(
-                            lineWidth: 2,
-                            dash: [5])
-                        )
-                        .foregroundStyle(.appSecondary.opacity(0.4))
+                        .fill(.appPrimary)
                         .overlay {
-                            Image(systemName: "plus")
-                                .font(.themeTitle3())
-                                .foregroundStyle(.appSecondary)
+                            VStack(alignment: .leading){
+                                Text(journal.title)
+                                    .font(.themeTitle3(weight: .heavy))
+                                Text(journal.content)
+                                    .font(.themeBody())
+                            }
+                            .frame(
+                                maxWidth: .infinity,
+                                alignment: Alignment(
+                                    horizontal: .leading,
+                                    vertical: .center
+                                )
+                            ).padding(.horizontal, 25)
                         }
                         .frame(height: 100)
                         .padding(.horizontal, 15)
                         .padding(.top, 10)
                 }
-                
-                // MARK: List All Journal
-                ForEach(journalViewModel.data, id: \.self){ journal in
-                    Button(action: {
-                        
-                    }){
-                        RoundedRectangle(cornerSize: CGSize(width: 30, height: 30))
-                            .fill(.appPrimary)
-                            .overlay {
-                                VStack(alignment: .leading){
-                                    Text(journal.title)
-                                        .font(.themeTitle3(weight: .heavy))
-                                    Text(journal.content)
-                                        .font(.themeBody())
-                                }
-                                .frame(
-                                    maxWidth: .infinity,
-                                    alignment: Alignment(
-                                        horizontal: .leading,
-                                        vertical: .center
-                                    )
-                                ).padding(.horizontal, 25)
-                            }
-                            .frame(height: 100)
-                            .padding(.horizontal, 15)
-                            .padding(.top, 10)
-                    }
-                }
             }
         }
+        .frame(
+            maxHeight: .infinity,
+            alignment: Alignment(
+                horizontal: .leading,
+                vertical: .top
+            )
+        )
         .foregroundStyle(.appSecondary)
         .onAppear {
+            journalViewModel.fetch()
             relaxViewModel.$isJournaling.sink(receiveValue: {value in
                 Timer.scheduledTimer(withTimeInterval: 1, repeats: false){_ in
                     showTimerNotification = value
